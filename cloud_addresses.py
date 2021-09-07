9684d21340326f32de3c77cedcd908afd9137ffc
Jason E. Robinson <jason.robinson@auspex-labs.com>
2021-09-06 20:29:25 -0400

    Code improvements

--- cloud_addresses.py
+++ cloud_addresses.py
@@ -1,9 +1,8 @@
 import re
 import json
-import requests
-
-
 from ipaddress import ip_network
+
+import requests
 
 AWS_SOURCE = "https://ip-ranges.amazonaws.com/ip-ranges.json"
 
@@ -20,35 +19,19 @@ IPV4_FILE = "cloud_networks_4.json"
 IPV6_FILE = "cloud_networks_6.json"
 
 
-def reduce_subnets(prefixes):
-
-    # Remove CIDRs that are subnets of other CIDRs.
-
-    cidr = list(prefixes)
-    cidr.sort()
-
-    for net in cidr.copy():
-        for sub in cidr.copy():
-            if net != sub:
-                if ip_network(net).subnet_of(ip_network(sub)):
-                    cidr.remove(net)
-
-    return cidr
-
-
 def aws(url=AWS_SOURCE):
 
     aws_ranges = json.loads(requests.get(url).content)
 
-    ipv4prefixes = set()
+    aws_ipv4prefixes = set()
     for prefix in aws_ranges["prefixes"]:
-        ipv4prefixes.add(ip_network(prefix["ip_prefix"]))
+        aws_ipv4prefixes.add(ip_network(prefix["ip_prefix"]))
 
-    ipv6prefixes = set()
+    aws_ipv6prefixes = set()
     for prefix in aws_ranges["ipv6_prefixes"]:
-        ipv6prefixes.add((ip_network(prefix["ipv6_prefix"])))
+        aws_ipv6prefixes.add((ip_network(prefix["ipv6_prefix"])))
 
-    return ipv4prefixes, ipv6prefixes
+    return aws_ipv4prefixes, aws_ipv6prefixes
 
 
 def azure(url=AZURE_SOURCE):
@@ -57,34 +40,34 @@ def azure(url=AZURE_SOURCE):
 
     azure_ranges = json.loads(requests.get(re.findall(r"https://download.*?\.json", azure_address_page.text)[0]).content)
 
-    ipv4prefixes = set()
-    ipv6prefixes = set()
+    az_ipv4prefixes = set()
+    az_ipv6prefixes = set()
 
     for prefix in azure_ranges["values"]:
         for network in prefix["properties"]["addressPrefixes"]:
             net = ip_network(network)
             if net.version == 4:
-                ipv4prefixes.add(net)
+                az_ipv4prefixes.add(net)
             elif net.version == 6:
-                ipv6prefixes.add(net)
+                az_ipv6prefixes.add(net)
             else:
                 continue
 
-    return ipv4prefixes, ipv6prefixes
+    return az_ipv4prefixes, az_ipv6prefixes
 
 
 def gpc(url=GPC_SOURCE):
 
     gpc_ranges = json.loads(requests.get(url).content)
 
-    ipv4prefixes = set()
-    ipv6prefixes = set()
+    gpc_ipv4prefixes = set()
+    gpc_ipv6prefixes = set()
 
     for prefix in gpc_ranges["prefixes"]:
         if prefix.get("ipv4Prefix") is not None:
-            ipv4prefixes.add(ip_network(prefix.get("ipv4Prefix")))
+            gpc_ipv4prefixes.add(ip_network(prefix.get("ipv4Prefix")))
         if prefix.get("ipv6Prefix") is not None:
-            ipv6prefixes.add(ip_network(prefix.get("ipv6Prefix")))
+            gpc_ipv6prefixes.add(ip_network(prefix.get("ipv6Prefix")))
 
     return ipv4prefixes, ipv6prefixes
 
@@ -93,35 +76,35 @@ def ocean(url=OCEAN_SOURCE):
 
     ocean_ranges = requests.get(url).content
 
-    ipv4prefixes = set()
-    ipv6prefixes = set()
+    do_ipv4prefixes = set()
+    do_ipv6prefixes = set()
 
     for prefix in ocean_ranges.splitlines():
         net = ip_network(prefix.decode("utf-8").split(",")[0])
         if net.version == 4:
-            ipv4prefixes.add(net)
+            do_ipv4prefixes.add(net)
         elif net.version == 6:
-            ipv6prefixes.add(net)
+            do_ipv6prefixes.add(net)
         else:
             continue
 
-    return ipv4prefixes, ipv6prefixes
+    return do_ipv4prefixes, do_ipv6prefixes
 
 
 def oracle(url=ORACLE_SOUCE):
 
     oracle_ranges = json.loads(requests.get(url).content)
 
-    ipv4prefixes = set()
-    ipv6prefixes = set()
+    orc_ipv4prefixes = set()
+    orc_ipv6prefixes = set()
 
     for cidrs in oracle_ranges["regions"]:  # TODO Needs better variable names
         for cidr in cidrs["cidrs"]:
             net = ip_network(cidr["cidr"])
             if net.version == 4:
-                ipv4prefixes.add(net)
+                orc_ipv4prefixes.add(net)
             elif net.version == 6:
-                ipv6prefixes.add(net)
+                orc_ipv6prefixes.add(net)
             else:
                 continue
 
@@ -137,15 +120,16 @@ def merge_networks(prefixes):
     # Find and merge adjacent CIDRs.
 
     networks = dict()
-    [networks.update({(int(x.split("/")[1])): []}) for x in cidr if int(x.split("/")[1]) not in networks.keys()]
 
     for net in cidr:
+        if int(net.split("/")[1]) not in networks.keys():
+            networks.update({(int(net.split("/")[1])): []})
         networks[int(net.split("/")[1])].append(net)
 
     updates = True
     while updates:
         updates = False
-        for mask in sorted(networks.copy()):
+        for mask in sorted(networks.copy(), reverse=False):
             for network in networks[mask].copy():
                 complete = True
                 for sub in ip_network(network).supernet().subnets():
@@ -161,11 +145,9 @@ def merge_networks(prefixes):
                     for sub in ip_network(network).supernet().subnets():
                         networks[mask].remove(str(sub))
 
-    net_count = 0
-    for net in networks:
-        net_count += len(networks[net])
-
-    print("Start Subnets:", net_count)
+    # net_count = 0
+    # for net in networks:
+    #     net_count += len(networks[net])
 
     return networks
 
@@ -174,8 +156,6 @@ def write_networks(networks, network_file):
 
     with open(network_file, "w") as open_file:
         json.dump(networks, open_file, indent=4, sort_keys=True)
-
-    return
 
 
 ipv4prefixes = set()
@@ -202,5 +182,8 @@ ipv6prefixes.update(oracle6)
 ipv4nets = merge_networks(ipv4prefixes)
 ipv6nets = merge_networks(ipv6prefixes)
 
-write_networks(ipv4nets, IPV4_FILE)
-write_networks(ipv6nets, IPV6_FILE)
+print(len(ipv4nets))
+
+
+# write_networks(ipv4nets, IPV4_FILE)
+# write_networks(ipv6nets, IPV6_FILE)
